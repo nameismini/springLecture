8f25574467bcd9e448e38a9ecf6414864e0d2de2
nameismini <nameismini@gmail.com>
2025-04-15 08:34:57 +0900

    thymeleaf 기능 테스트

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -96,6 +96,35 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    private void addUsers(Model model) {
+
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+    }
+
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
